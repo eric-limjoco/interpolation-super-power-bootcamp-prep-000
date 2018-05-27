f5ec2f75856c64d6a0c269370a47d58f25b5df78
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-27 14:44:00 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,5 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[0]}, I: #{colors[0]}, V: #{colors[0]}"
+end
